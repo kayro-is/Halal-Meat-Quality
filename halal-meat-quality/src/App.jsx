0af33a7606cb4c8df2b4d8faa54400373a00eca5
kayro-is <luffykosuki@gmail.com>
2024-07-12 16:20:49 +0200

    crtéation de Mission

--- halal-meat-quality/src/App.jsx
+++ halal-meat-quality/src/App.jsx
@@ -1,12 +1,18 @@
 import './App.scss'
 import Hero from './Components/Hero/Hero';
+import Mission from './Components/Mission/Mission';
 import NavBar from './Components/NavBar/NavBar'
 
 const App = () => {
   return (
     <div>
+      <section id="page d'accueil">
     <NavBar />
     <Hero />
+    </section>
+    <section id='service'>
+     <Mission />
+    </section>
     </div>
   );
 }
---
+++ halal-meat-quality/src/Components/Mission/Mission.jsx
@@ -0,0 +1,46 @@
+import React from 'react';
+import './Mission.scss'
+
+const Mission = () => {
+  return (
+    <div className='parallax'>
+        <div className='title'>
+        <h1>Nuestra misión</h1>
+        </div>
+        <div className="listContainer" >
+        <div className="box">
+
+                        <h2>Al servicio de los consumidores</h2>
+                        <p>
+                        Para garantizar a los consumidores el carácter halal de los productos, 
+                        nuestros empleados están presentes a lo largo de toda la cadena,
+                         desde el sacrificio del animal hasta el embalaje y el sellado de los productos.
+                        </p>
+                        
+                    </div>
+                    <div className="box">
+
+                        <h2>Preservar el halal en el entorno industrial</h2>
+                        <p>
+                        Los mataderos industriales son hoy en día espacios confinados prohibidos al público en general.
+                         Por lo tanto, los consumidores no tienen ninguna visión de la manera en que los animales son tratados y sacrificados.
+                        </p>
+                
+                    </div>
+                    <div className="box">
+
+                        <h2>Desarrollar una certificación <br />desinteresada</h2>
+                        <p>
+                        Los consumidores están cada vez más preocupados por las condiciones de vida y de sacrificio de los animales destinados al consumo.
+                         Una certificación independiente permitiría tranquilizar a los consumidores al ofrecerles una garantía objetiva del 
+                         cumplimiento de las normas éticas y sanitarias.
+                        </p>
+                        
+                    </div>
+                </div>
+        
+    </div>
+  );
+}
+
+export default Mission;
