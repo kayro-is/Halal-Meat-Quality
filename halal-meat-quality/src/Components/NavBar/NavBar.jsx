529cb1a7bd6f14d918b142b436ae92a5c5332a1b
kayro-is <luffykosuki@gmail.com>
2024-07-12 11:49:48 +0200

    responsive navBar

--- halal-meat-quality/src/Components/NavBar/NavBar.jsx
+++ halal-meat-quality/src/Components/NavBar/NavBar.jsx
@@ -1,22 +1,30 @@
 import './NavBar.scss'
-
-import React from 'react';
+import React, { useState } from 'react';
 
 const NavBar = () => {
+  const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
+
+  const toggleMobileMenu = () => {
+    setIsMobileMenuOpen(!isMobileMenuOpen);
+  }
+
   return (
     <header className='header'>
-        <div className='logo'>
+      <div className='logo'>
         <img src="/HMQ.png" alt="Logo" />
-        </div>
-        <nav className='navbar'>
-           <a href="/">Inicio</a>
-           <a href="/">Presentación</a>
-           <a href="/">Nuestros Socios</a>
-           <a href="/">Certificación</a>
-           <a href="/">Contacto</a>
-
-        </nav>
-
+      </div>
+      <nav className={`navbar ${isMobileMenuOpen ? 'open' : ''}`}>
+        <a href="/">Inicio</a>
+        <a href="/">Presentación</a>
+        <a href="/">Nuestros Socios</a>
+        <a href="/">Certificación</a>
+        <a href="/">Contacto</a>
+      </nav>
+      <div className='burger' onClick={toggleMobileMenu}>
+        <div className='line1'></div>
+        <div className='line2'></div>
+        <div className='line3'></div>
+      </div>
     </header>
   );
 }
